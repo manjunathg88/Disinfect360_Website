59478944e1e5f440bbb71cece4eaf44c1d8c8a13
Manjunath Gangadhar <manjunathg88@gmail.com>
2024-01-05 19:36:58 +0530

    Add files via upload

---
+++ Disinfect360/README.md
@@ -0,0 +1,1 @@
+This project contains a simple HTML5 website created for a vendor. I have utilized bootstrap templates and googleapis for fonts.
---
+++ Disinfect360/Website/index.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "info@disinfect360.in";
+$subject = $_POST['subject'];
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "$recipient, $subject, $formcontent, $mailheader";
+?>
